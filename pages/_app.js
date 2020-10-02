ba40effe8e1ced72db28c9b8899f3e0d521cce0d
Andy Hong <andyhongoc@gmail.com>
2020-10-01 17:08:05 -0700

    added fathom analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,27 @@
 import '../styles/globals.css'
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+import * as Fathom from 'fathom-client'
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter()
+
+  useEffect(() => {
+    Fathom.load("NEHIWDNL", {
+      includedDomains: ["tempest.cards"]
+    })
+
+    function onRouteChangeComplete() {
+      Fathom.trackPageview()
+    }
+
+    router.events.on('routeChangeComplete', onRouteChangeComplete)
+
+    return () => {
+      router.events.off('routeChangeComplete', onRouteChangeComplete)
+    }
+  }, [])
+
   return <Component {...pageProps} />
 }
 
